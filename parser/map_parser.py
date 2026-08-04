ba2c2124aad4e9013189dd774b5bfe035aad9d57
Jay Krishnan <jaykrishnan1997@gmail.com>
2026-08-04 20:28:20 +0200

    parser started

--- parser/map_parser.py
+++ parser/map_parser.py
@@ -4,11 +4,50 @@
 #                                                          :::      ::::::::  #
 #   map_parser.py                                        :+:      :+:    :+:  #
 #                                                      +:+ +:+         +:+    #
-#   By: jkrishna <jkrishna@student.42.fr>            +#+  +:+       +#+       #
+#   By: jay-k <jay-k@student.42.fr>                  +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/08/03 14:51:56 by jkrishna            #+#    #+#            #
-#   Updated: 2026/08/03 15:07:32 by jkrishna           ###   ########.fr      #
+#   Updated: 2026/08/04 20:28:07 by jay-k              ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
-# class
+# import re
+# from data_models.connection import Connection
+# from data_models.graph import Graph
+# from data_models.zone import Zone
+# from typing import List
+
+class Parser():
+    def __init__():
+        pass
+# """Class responsible for parsing map files into a Graph object."""
+# """
+#         Parses a zone definition line and extracts metadata.
+#         Its only job is to convert a list of words into a Zone object.
+
+#         Args:
+#             tokens: A list of strings representing the split line
+#             from the file.
+
+#         Returns:
+#             A Zone object populated with the parsed data.
+
+#         Raises:
+#             ValueError: If an invalid zone type is encountered.
+#         """
+#         """Reads a file and constructs the Graph
+#             representing the drone network.
+
+#         Args:
+#             filepath: Path to the input map file.
+
+#         Returns:
+#             A fully initialized Graph object.
+
+#         Raises:
+#             ValueError: If parsing fails due to syntax errors, missing hubs,
+#                 duplicate connections, or invalid drone counts.
+#             FileNotFoundError: If the specified file does not exist.
+#         """
+#         """Parses and validates a positive integer within MAX_VALUE."""
+#         """Registers a zone and checks for duplicates."""
